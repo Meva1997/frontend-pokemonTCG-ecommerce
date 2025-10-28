1f734946e133fecbd03b2fc05f5d2d9926362a17
Alex Medina <alexmedval2@gmail.com>
2025-10-28 10:36:22 -0600

    installed vercel analytics and added to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -24,6 +25,7 @@ export default async function RootLayout({
           <Footer />
         </div>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
